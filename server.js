835a78a0623e60f6a9fae0f1658d14731f0181a1
Alejandro Guerrero <alexgue404@gmail.com>
2021-06-11 13:35:40 +0200

    feat: New logs fix

--- server.js
+++ server.js
@@ -19,109 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 'use strict';
-const governify = require('governify-commons');
-const config = governify.configurator.getConfig('main');
 
-// Add this to the VERY top of the first file loaded in your app
-var apm = require('elastic-apm-node').start({
-  // Override service name from package.json
-  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-  // Override service name from package.json
-  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-  serviceName: 'Registry',
-  serviceNodeName: 'Registry',
-  captureBody: 'all',
-  transactionMaxSpans: -1,
-  usePathAsTransactionName: true,
-  abortedErrorThreshold: 0,
-  distributedTracingOrigins: ['*'],
-  active: config.telemetry.enabled
-
-});
-
-/*
- * Put here your dependencies
- */
-const http = require('http'); // Use http if your app will be behind a proxy.
-const https = require('https'); // Use https if your app will not be behind a proxy.
-const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-const fs = require('fs');
-const path = require('path');
-
-// Self dependencies
-const logger = governify.getLogger().tag('deploy');
-const db = require('./src/database');
-const swaggerUtils = require('./src/utils').swagger;
-const middlewares = require('./src/utils').middlewares;
-
-const server = null;
-const app = express();
-
-const frontendPath = path.join(__dirname, '/public');
-const serverPort = process.env.PORT || config.server.port;
-const CURRENT_API_VERSION = 'v6';
-
-app.use(express.static(frontendPath));
-
-// Default server options
-app.use(compression());
-
-logger.info("Using '%s' as HTTP body size", config.server.bodySize);
-app.use(
-  bodyParser.urlencoded({
-    limit: config.server.bodySize,
-    extended: 'true'
-  })
-);
-
-app.use(
-  bodyParser.json({
-    limit: config.server.bodySize,
-    type: 'application/json'
-  })
-);
-
-// Configurable server options
-
-if (config.server.bypassCORS) {
-  logger.info("Adding 'Access-Control-Allow-Origin: *' header to every path.");
-  app.use(cors());
-}
-
-if (config.server.useHelmet) {
-  logger.info('Adding Helmet related headers.');
-  app.use(helmet());
-}
-
-if (config.server.httpOptionsOK) {
-  app.options('/*', function (req, res) {
-    logger.info('Bypassing 405 status put by swagger when no request handler is defined');
-    return res.sendStatus(200);
-  });
-}
-
-if (config.server.servePackageInfo) {
-  app.use('/api/info', function (req, res) {
-    logger.debug("Serving package.json at '%s'", '/api/info');
-    res.json(require('./package.json'));
-  });
-}
-
-// middleware to control when an agreement state process is already in progress
-
-app.use('/api/v6/states/:agreement', middlewares.stateInProgress);
-
-// latest documentation redirection
-app.use('/api/latest/docs', function (req, res) {
-  res.redirect('/api/' + CURRENT_API_VERSION + '/docs');
-});
-app.use('/api/latest/api-docs', function (req, res) {
-  res.redirect('/api/' + CURRENT_API_VERSION + '/api-docs');
-});
 
 /**
  * Registry module.
@@ -145,8 +43,109 @@ module.exports = {
  * @param {function} callback callback function
  * @alias module:registry.deploy
  * */
-function _deploy (configurations, commonsMiddleware, callback) {
-  app.use('/commons', commonsMiddleware);
+function _deploy(configurations, commonsMiddleware, callback) {
+  const governify = require('governify-commons');
+  const config = governify.configurator.getConfig('main');
+
+  // Add this to the VERY top of the first file loaded in your app
+  var apm = require('elastic-apm-node').start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'Registry',
+    serviceNodeName: 'Registry',
+    captureBody: 'all',
+    transactionMaxSpans: -1,
+    usePathAsTransactionName: true,
+    abortedErrorThreshold: 0,
+    distributedTracingOrigins: ['*'],
+    active: config.telemetry.enabled
+
+  });
+
+  const http = require('http'); // Use http if your app will be behind a proxy.
+  const https = require('https'); // Use https if your app will not be behind a proxy.
+  const bodyParser = require('body-parser');
+  const express = require('express');
+  const cors = require('cors');
+  const helmet = require('helmet');
+  const compression = require('compression');
+  const fs = require('fs');
+  const path = require('path');
+
+  // Self dependencies
+  const logger = governify.getLogger().tag('deploy');
+  const db = require('./src/database');
+  const swaggerUtils = require('./src/utils').swagger;
+  const middlewares = require('./src/utils').middlewares;
+
+  const server = null;
+  const app = express();
+
+  const frontendPath = path.join(__dirname, '/public');
+  const serverPort = process.env.PORT || config.server.port;
+  const CURRENT_API_VERSION = 'v6';
+
+  app.use(express.static(frontendPath));
+
+  // Default server options
+  app.use(compression());
+
+  logger.info("Using '%s' as HTTP body size", config.server.bodySize);
+  app.use(
+    bodyParser.urlencoded({
+      limit: config.server.bodySize,
+      extended: 'true'
+    })
+  );
+
+  app.use(
+    bodyParser.json({
+      limit: config.server.bodySize,
+      type: 'application/json'
+    })
+  );
+
+  // Configurable server options
+
+  if (config.server.bypassCORS) {
+    logger.info("Adding 'Access-Control-Allow-Origin: *' header to every path.");
+    app.use(cors());
+  }
+
+  if (config.server.useHelmet) {
+    logger.info('Adding Helmet related headers.');
+    app.use(helmet());
+  }
+
+  if (config.server.httpOptionsOK) {
+    app.options('/*', function (req, res) {
+      logger.info('Bypassing 405 status put by swagger when no request handler is defined');
+      return res.sendStatus(200);
+    });
+  }
+
+  if (config.server.servePackageInfo) {
+    app.use('/api/info', function (req, res) {
+      logger.debug("Serving package.json at '%s'", '/api/info');
+      res.json(require('./package.json'));
+    });
+  }
+
+  // middleware to control when an agreement state process is already in progress
+
+  app.use('/api/v6/states/:agreement', middlewares.stateInProgress);
+
+  // latest documentation redirection
+  app.use('/api/latest/docs', function (req, res) {
+    res.redirect('/api/' + CURRENT_API_VERSION + '/docs');
+  });
+  app.use('/api/latest/api-docs', function (req, res) {
+    res.redirect('/api/' + CURRENT_API_VERSION + '/api-docs');
+  });
+
+  app.use(commonsMiddleware);
   logger.info('Trying to deploy server');
   if (configurations) {
     logger.info('Reading configuration...');
@@ -197,7 +196,7 @@ function _deploy (configurations, commonsMiddleware, callback) {
  * @param {function} callback callback function
  * @alias module:registry.undeploy
  * */
-function _undeploy (callback) {
+function _undeploy(callback) {
   if (db) {
     db.close(function () {
       server.close(function () {
